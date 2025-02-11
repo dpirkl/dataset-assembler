527660f14c4762871a95e8717a79499fdd72692a
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2025-02-11 11:44:00 +0100

    made compile_inplace the default and removed old function

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import argparse
 
-from helper import compile_all, compile_inplace, create_dataset_file, download, extract_c_files
+from helper import compile_all, create_dataset_file, download, extract_c_files
 
 if __name__ == "__main__":
     # add arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--copy", action="store_true")
+    parser.add_argument("-c", "--copy", action="store_true", help="Copys C files to specified folder.")
     args = parser.parse_args()
 
     download()
     if args.copy:
-        assembly_dir = "assembly"
         extract_c_files()
-        compile_all()
-    else:
-        compile_inplace()
-        assembly_dir = "assembly-inplace"
-    create_dataset_file(source_dir=assembly_dir)
+    compile_all()
+    create_dataset_file()

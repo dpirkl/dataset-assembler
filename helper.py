9687125117ed4bb4c0eca1dc0dfb6b3654b57589
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2025-02-11 12:31:48 +0100

    general improvements
    added documentation to compile function
    added command line arguments for main.py
    added seperate uncompress file function
    renamed extract to copy
    fixed bugs in compile function
    renamed assembler to compiler

--- helper.py
+++ helper.py
@@ -1,62 +1,61 @@
 import os
 import shutil
-import time
 
 from tqdm import tqdm
 
 
-def download(source_dir="sources", source_file="sources.txt", verbose=False):
-    with open(os.path.join(source_dir, source_file), "r") as f:
+def uncompress_files(directory:str="sources", verbose:bool=True):
+    """Searches for compressed files/folder in `directory` and uncompresses them.
+
+    Args:
+        directory (str, optional): Directory to serach for compressed files. Also searches subdirectories. Defaults to "sources".
+        verbose (bool, optional): Controlls verbosity. Defaults to True.
+    """
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.7z'):
+                os.system(f"7z x {os.path.join(root, file)} -o{directory} -aoa{" >/dev/null 2>/dev/null" if not verbose else ""}")
+
+            # TODO: Add other compressed file types
+
+
+def download(source_dir="sources", sources_file="sources.txt", verbose=False):
+    with open(sources_file, "r") as f:
         links = f.readlines()
 
     for link in tqdm(links, desc="Downloads"):
-        link = link.strip()
+        link = link.strip()  # noqa: PLW2901
 
         if link.startswith("wget"):
             file_name = link.split('/')[-1]
             if not os.path.exists(os.path.join(source_dir, file_name)):
-                command = f"{link} -P ./sources"
-                if not verbose:
-                    command += " >/dev/null 2>/dev/null"
-                os.system(command)
+                os.system(f"{link} -P {source_dir}{" >/dev/null 2>/dev/null" if not verbose else ""}")
             else:
                 if verbose:
-                    print(f"A file with the name {file_name} already exists at {os.path.join('./sources', file_name)}. Continuing with the next link.")
+                    print(f"A file with the name {file_name} already exists at {os.path.join(source_dir, file_name)}. Continuing with the next link.")
 
         else:
             folder_name = os.path.join(source_dir, link.split('/')[-1][:-4])
             if not os.path.exists(folder_name):
-                command = f"git clone {link} {folder_name}"
-                if not verbose:
-                    command += " >/dev/null 2>/dev/null"
-                os.system(command)
+                os.system(f"git clone {link} {folder_name}{" >/dev/null 2>/dev/null" if not verbose else ""}")
             else:
                 if verbose:
                     print(f"A folder with the name {link.split('/')[-1][:-4]} already exists at {folder_name} already exists. Continuing with the next link.")
 
-def extract_c_files(source_dir="sources", dest_dir="extracted", verbose=False):
-    # Create the destination directory if it doesn't exist
+    uncompress_files(source_dir)
+
+
+def copy_c_files(source_dir="sources", dest_dir="extracted", verbose=False):
+
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
-
-    # Walk through the source directory
-    for root, _, files in os.walk(source_dir):
-        for file in files:
-            if file.endswith('.7z'):
-                command = f"7z x {os.path.join(root, file)} -o{source_dir} -aoa"
-                if not verbose:
-                    command += " >/dev/null 2>/dev/null"
-                os.system(command)
 
     for root, _, files in os.walk(source_dir):
         for file in files:
             if file.endswith('.c'):
-                # Construct the full file path
                 file_path = os.path.join(root, file)
-                file_name = file_path[10:].replace("/", "_").replace(" ", "").replace("(", "_").replace(")", "_").lower()
-
-                # Copy the file to the destination directory
-                # shutil.copy2(file_path, destination_dir)
+                file_name = file_path[10:].replace("/", "_").replace(" ", "")
                 dest = os.path.join(dest_dir, file_name)
                 shutil.copy(file_path, dest)
 
@@ -66,14 +65,13 @@ def replace_tab(code:str, replace_with:str=" "):
 
 
 def create_dataset_file(source_dir="assembly", dest_file:str="dataset.txt", eos:str="<|endoftext|>"):
-    # create emtpy dataset.txt file. if it exists, delete it
+
     if os.path.exists(dest_file):
         os.remove(dest_file)
 
     for filename in tqdm(os.listdir(source_dir), desc="Creating dataset file"):
         if filename.lower().endswith(".s"):
             with open(f"{os.path.join(source_dir, filename)}", "r") as f:
-                # lines = f.readlines()
                 code = f.read()
 
             code = replace_tab(code)
@@ -83,35 +81,24 @@ def create_dataset_file(source_dir="assembly", dest_file:str="dataset.txt", eos:
             with open(dest_file, "a") as f:
                 f.write(code)
 
-        else:
-            raise RuntimeWarning(f"File in source folder with wrong extension. Should be .S, but is {filename.split('.')[-1]}")
 
-def compile_all(source_dir="sources", dest_dir="assembly", assembler="gcc", riscv:bool=True, verbose:bool=False):
+def compile_all(source_dir:str="sources", dest_dir:str="assembly", compiler:str="gcc", riscv:bool=True, verbose:bool=False):
+    """Compile all C files in `source_dir`.
+
+    Args:
+        source_dir (str, optional): Directory to serach for C files. Defaults to "sources".
+        dest_dir (str, optional): Directory to save assembly files. Defaults to "assembly".
+        compiler (str, optional): Name of compiler. Defaults to "gcc".
+        riscv (bool, optional): Controlls use of RISC-V version of compiler. Defaults to True.
+        verbose (bool, optional): Controlls verbosity. Defaults to False.
+    """
     if riscv:
-        assembler = "riscv64-unknown-elf-" + assembler
+        compiler = "riscv64-unknown-elf-" + compiler
 
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
+    for file in tqdm([os.path.join(root, f) for root, _, files in os.walk(source_dir) for f in files], desc="Compiling"):
+        if file.endswith(".c") or file.endswith(".C"):
+            file = file.replace(" ", "")  # noqa: PLW2901
+            s_filename = file[len(source_dir)+1:-2].replace("/", "_") + ".S"
+            s_file = os.path.join(dest_dir, s_filename)
 
-    # Walk through the source directory
-    for root, _, files in os.walk(source_dir):
-        for file in files:
-            if file.endswith('.7z'):
-                command = f"7z x {os.path.join(root, file)} -o{source_dir} -aoa"
-                if not verbose:
-                    command += " >/dev/null 2>/dev/null"
-                os.system(command)
-
-    for file in tqdm([os.path.join(root, f) for root, _, files in os.walk('sources') for f in files], desc="Compiling"):
-        for file in files:
-            if file.endswith(".c"):
-                c_file = os.path.join(source_dir, file)
-                s_filename = file[:-2] + ".S"
-                s_file = os.path.join(dest_dir, s_filename)
-
-                command = f"{assembler} -O0 -S {c_file} -o {s_file}"
-                if not verbose:
-                        command += " >/dev/null 2>/dev/null"
-                os.system(command)
-            else:
-                raise RuntimeError("Wrong file format in extracted directory.")
+            os.system(f"{compiler} -O0 -S '{file}' -o '{s_file}'{" >/dev/null 2>/dev/null" if not verbose else ""}")
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import argparse
 
-from helper import compile_all, create_dataset_file, download, extract_c_files
+from helper import compile_all, copy_c_files, create_dataset_file, download
 
 if __name__ == "__main__":
     # add arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--copy", action="store_true", help="Copys C files to specified folder.")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("--source_dir", default="sources")
+    parser.add_argument("--sources_file", default="sources/sources.txt")
+    parser.add_argument("--copy_dir", default="copy")
+    parser.add_argument("--assembly_dir", default="assembly")
     args = parser.parse_args()
 
-    download()
+    source_dir = args.source_dir
+    sources_file = args.sources_file
+    copy_dir = args.copy_dir
+    assembly_dir = args.assembly_dir
+
+    verbose = args.verbose
+
+    download(source_dir=source_dir, sources_file=sources_file, verbose=verbose)
     if args.copy:
-        extract_c_files()
-    compile_all()
-    create_dataset_file()
+        copy_c_files(source_dir=source_dir, dest_dir=copy_dir, verbose=verbose)
+    compile_all(source_dir=source_dir, verbose=verbose)
+    create_dataset_file(source_dir=source_dir, verbose=verbose)

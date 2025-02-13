9212c4545c2a42260db8efde46400a5acd929680
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2025-02-13 01:32:21 +0100

    add tokenization
    code can now be tokenized and filtered to only contain instructions
    cli arguments have been added to controll this behaviour

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 from tqdm import tqdm
+from transformers import AutoTokenizer
 
 
 def uncompress_files(directory:str="sources", verbose:bool=False):
@@ -73,12 +74,21 @@ def copy_c_files(source_dir:str="sources", dest_dir="copy"):
                 shutil.copy(file_path, dest)
 
 
-def replace_tab(code:str, replace_with:str=" "):
+def replace_tab(code:str, n_spaces:int=1):
     """Replace tabs with spaces in code."""
-    return code.replace("\t", replace_with)
+    return code.replace("\t", " "*n_spaces)
+
+def filter_code(code:str):
+    lines = code.split("\n")
+    filtered_lines = []
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if line == "" or line[0]=="." or line[0]=="#": continue
+        filtered_lines.append(line)
+    return "\n".join(filtered_lines)
 
 
-def create_dataset_file(assembly_dir:str="assembly", dest_file:str="dataset.txt", eos:str="<|endoftext|>"):
+def create_dataset_file(assembly_dir:str="assembly", dataset_file:str="dataset.txt", eos:str="<|endoftext|>", full_code:bool=False, toknized_dataset_file:str="dataset_tokenized.txt", skip_tokenize:bool=False, model_name:str="Qwen/Qwen2.5-Coder-0.5"):
     """Creates a contiuos dataset file from all assembly files in `source_dir`.
 
     Args:
@@ -87,20 +97,45 @@ def create_dataset_file(assembly_dir:str="assembly", dest_file:str="dataset.txt"
         eos (str, optional): Sperator between files. Defaults to "<|endoftext|>".
     """
 
-    if os.path.exists(dest_file):
-        os.remove(dest_file)
+    if os.path.exists(dataset_file):
+        os.remove(dataset_file)
 
-    for filename in tqdm(os.listdir(assembly_dir), desc="Creating dataset file"):
+    if not skip_tokenize:
+        tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-0.5B")
+        dest_tokenize_file:str = dataset_file.split(".")[0] + "_tokenized.txt"
+        if os.path.exists(dest_tokenize_file):
+            os.remove(dest_tokenize_file)
+
+    for idx, filename in tqdm(enumerate(os.listdir(assembly_dir)), desc="Creating dataset file()"):
         if filename.lower().endswith(".s"):
             with open(f"{os.path.join(assembly_dir, filename)}", "r") as f:
                 code:str = f.read()
 
-            code = replace_tab(code)
+            code = replace_tab(code, n_spaces=1)
+
+            if len(code) > 6000:
+                #TODO: Add a better way to handle long files
+                print(f"Skipping {filename} as it is too long.")
+                continue
+
+            if not full_code:
+                full_code_file:str = dataset_file.split(".")[0] + "_full_code.txt"
+                with open(full_code_file, "a") as f:
+                    f.write(code)
+                code = filter_code(code)
             code += eos
             code = repr(code)[1:-1]
 
-            with open(dest_file, "a") as f:
+            with open(dataset_file, "a") as f:
                 f.write(code)
+
+            if not skip_tokenize:
+                tokenized_code = tokenizer.encode(code)
+                tokenized_code = " ".join([str(token) for token in tokenized_code])
+
+
+                with open(dest_tokenize_file, "a") as f:
+                    f.write(tokenized_code)
 
 
 def compile_all(source_dir:str="sources", dest_dir:str="assembly", compiler:str="gcc", copy:bool=False, copy_dir:str="copy", riscv:bool=True, verbose:bool=False):
--- main.py
+++ main.py
@@ -4,16 +4,32 @@ from helper import compile_all, create_dataset_file, download
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--copy", action="store_true", help="Copys C files to specified folder.")
     parser.add_argument("-v", "--verbose", action="store_true", help="Prints extra information.")
+    parser.add_argument("--full_code", action="store_true", help="Store full code in dataset and not only instructions.")
     parser.add_argument("--source_dir", default="sources", help="Directory for downloads.")
     parser.add_argument("--sources_file", default="sources/sources.txt", help="File with list of sources (links).")
+    parser.add_argument("-c", "--copy", action="store_true", help="Copys C files to specified folder.")
     parser.add_argument("--copy_dir", default="copy", help="Directory to copy C files to. Only used if -c/--copy is set.")
     parser.add_argument("--assembly_dir", default="assembly", help="Directory to store assembly files.")
     parser.add_argument("--dataset_file", default="dataset.txt", help="File to store dataset.")
+    parser.add_argument("--tokenized_dataset_file", default="dataset_tokenized.txt", help="File to store tokenized dataset.")
     parser.add_argument("--eos", default="<|endoftext|>", help="End of sequence token.")
+    parser.add_argument("--skip_tokenize", action="store_true", help="Skip tokenization.")
+    parser.add_argument("--model_name", default="Qwen/Qwen2.5-Coder-0.5", help="Model name for tokenizer. Uses Huggingface's transformers.")
+    parser.add_argument("--skip_download", action="store_true", help="Skip download.")
+    parser.add_argument("--skip_compile", action="store_true", help="Skip compile.")
     args = parser.parse_args()
 
-    download(source_dir=args.source_dir, sources_file=args.sources_file, verbose=args.verbose)
-    compile_all(source_dir=args.source_dir, copy=args.copy, copy_dir=args.copy_dir, verbose=args.verbose)
-    create_dataset_file(assembly_dir=args.assembly_dir, dest_file=args.dataset_file, eos=args.eos)
+    if not args.skip_download:
+        download(source_dir=args.source_dir, sources_file=args.sources_file, verbose=args.verbose)
+    if not args.skip_compile:
+        compile_all(source_dir=args.source_dir, copy=args.copy, copy_dir=args.copy_dir, verbose=args.verbose)
+    create_dataset_file(
+        assembly_dir=args.assembly_dir,
+        dataset_file=args.dataset_file,
+        toknized_dataset_file=args.tokenized_dataset_file,
+        eos=args.eos,
+        full_code=args.full_code,
+        skip_tokenize=args.skip_tokenize,
+        model_name=args.model_name
+    )
